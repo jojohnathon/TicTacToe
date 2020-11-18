2eaa5ee3bf4447c9b8e1ef79b340388022f605b1
BuildTools <unconfigured@null.spigotmc.org>
2020-11-17 23:06:06 -0800

    stuff

--- Main.java
+++ Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Board board = new Board();
+        while (true) {
+            board.display();
+            System.out.print("Player 1's turn:");
+            String playerOne = userInput.next();
 
-        board.display();
+            break Main;
+        }
+
     }
 }
